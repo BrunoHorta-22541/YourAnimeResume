046b2a2adaf39e1c25e8270f997011160517ecc8
BrunoHorta-22541 <22541@stu.ipbeja.pt>
2023-01-06 12:23:05 +0000

    contrutor e getters da classe anime

--- app/src/main/java/pt/estig/twdm/pdm/youranimeresume/Data/Anime.java
+++ app/src/main/java/pt/estig/twdm/pdm/youranimeresume/Data/Anime.java
@@ -6,4 +6,21 @@ public class Anime {
     private int currentwatchedepisode;
     //image
 
+    public Anime(String name, int totalepisodes, int currentwatchedepisode) {
+        this.name = name;
+        this.totalepisodes = totalepisodes;
+        this.currentwatchedepisode = currentwatchedepisode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTotalepisodes() {
+        return totalepisodes;
+    }
+
+    public int getCurrentwatchedepisode() {
+        return currentwatchedepisode;
+    }
 }
--- app/src/main/java/pt/estig/twdm/pdm/youranimeresume/Data/JAPIResponse.java
+++ app/src/main/java/pt/estig/twdm/pdm/youranimeresume/Data/JAPIResponse.java
@@ -5,11 +5,26 @@ import java.util.List;
 
 public class JAPIResponse {
     private List results;
+    private int count;
+    private String next;
+    private String previous;
 
-
-    public JAPIResponse(List results) {
-
+    public JAPIResponse(int count, String next, String previous,List results) {
+        this.count = count;
+        this.next = next;
+        this.previous = previous;
         this.results = results;
+    }
+    public int getCount() {
+        return count;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public String getPrevious() {
+        return previous;
     }
 
     public List getResults() {
